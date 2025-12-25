33d2fbf83be66957699d62e84d303db4fde6ddea
jzluo <20971593+jzluo@users.noreply.github.com>
2025-12-24 21:18:35 -0500

    add numba log1pexp

--- src/firthmodels/_numba/_utils.py
+++ src/firthmodels/_numba/_utils.py
@@ -9,3 +9,10 @@ def _expit(x: float) -> float:
         return 1.0 / (1.0 + z)
     z = np.exp(x)
     return z / (1.0 + z)
+
+
+@njit(fastmath=True)
+def _log1pexp(x: float) -> float:
+    if x > 0.0:
+        return x + np.log1p(np.exp(-x))
+    return np.log1p(np.exp(x))
--- tests/test_numba.py
+++ tests/test_numba.py
@@ -9,11 +9,19 @@ pytestmark = pytest.mark.skipif(not NUMBA_AVAILABLE, reason="numba not available
 if NUMBA_AVAILABLE:
     from firthmodels._numba._utils import (
         _expit,
+        _log1pexp,
     )
 
 
 @pytest.mark.parametrize(
-    "x", [-np.inf, -710, -100, -10, -1, 5, 0, 1, 5, 10, 100, 710, np.inf]
+    "x", [-np.inf, -710, -100, -10, -5, -1, 0, 1, 5, 10, 100, 710, np.inf]
 )
 def test_expit(x):
     assert _expit(x) == pytest.approx(expit(x), rel=1e-14, abs=1e-300)
+
+
+@pytest.mark.parametrize(
+    "x", [-np.inf, -710, -100, -10, -5, -1, 0, 1, 5, 10, 100, 710, np.inf]
+)
+def test_log1pexp(x):
+    assert _log1pexp(x) == pytest.approx(np.logaddexp(0, x), rel=1e-14)
