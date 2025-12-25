de3a74b98cdfbb1afd6d52936feb9ce21e2dc025
jzluo <20971593+jzluo@users.noreply.github.com>
2025-12-25 14:50:23 -0500

    Add cacheable BLAS/LAPACK intrinsics for numba backend
    The ctypes-based BLAS/LAPACK calls weren't able to be cached. Switched to llvmlite symbol registration + @intrinsic wrappers, which allows numba caching.
    add _blas_abi.py to read SciPy's capsule signatures for cross-platform ABI compatibility

--- src/firthmodels/_numba/linalg.py
+++ src/firthmodels/_numba/linalg.py
@@ -1,9 +1,12 @@
-import ctypes
-
 import numpy as np
+from llvmlite import binding as ll
+from llvmlite import ir
 from numba import njit
-from numba.extending import get_cython_function_address
+from numba.core import cgutils, types
+from numba.extending import get_cython_function_address, intrinsic
 from numpy.typing import NDArray
+
+from firthmodels._blas_abi import BLAS_FLAG_DTYPE, BLAS_INT_DTYPE
 
 
 @njit(inline="always")
@@ -12,271 +15,201 @@ def _alloc_f_order(rows: int, cols: int) -> NDArray[np.float64]:
     return temp.T
 
 
-# https://numba.discourse.group/t/extending-lapack-functions-into-numba/1394/4
+_SYMBOLS = {
+    "firth_dsyrk": ("scipy.linalg.cython_blas", "dsyrk"),
+    "firth_dgemm": ("scipy.linalg.cython_blas", "dgemm"),
+    "firth_dpotrf": ("scipy.linalg.cython_lapack", "dpotrf"),
+    "firth_dpotri": ("scipy.linalg.cython_lapack", "dpotri"),
+    "firth_dpotrs": ("scipy.linalg.cython_lapack", "dpotrs"),
+    "firth_dgetrf": ("scipy.linalg.cython_lapack", "dgetrf"),
+    "firth_dgetri": ("scipy.linalg.cython_lapack", "dgetri"),
+}
 
-_PTR = ctypes.POINTER
-_dble = ctypes.c_double
-_int = ctypes.c_int64
-
-_ptr_dbl = _PTR(_dble)
-_ptr_int = _PTR(_int)
-
-# BLAS/LAPACK pointers (scipy provides the cython-level wrappers).
-_dsyrk_addr = get_cython_function_address("scipy.linalg.cython_blas", "dsyrk")
-_dgemm_addr = get_cython_function_address("scipy.linalg.cython_blas", "dgemm")
-_dpotrf_addr = get_cython_function_address("scipy.linalg.cython_lapack", "dpotrf")
-_dpotri_addr = get_cython_function_address("scipy.linalg.cython_lapack", "dpotri")
-_dpotrs_addr = get_cython_function_address("scipy.linalg.cython_lapack", "dpotrs")
-_dgetrf_addr = get_cython_function_address("scipy.linalg.cython_lapack", "dgetrf")
-_dgetri_addr = get_cython_function_address("scipy.linalg.cython_lapack", "dgetri")
-
-_dsyrk_functype = ctypes.CFUNCTYPE(
-    None,
-    _ptr_int,  # uplo
-    _ptr_int,  # trans
-    _ptr_int,  # n
-    _ptr_int,  # k
-    _ptr_dbl,  # alpha
-    _ptr_dbl,  # a
-    _ptr_int,  # lda
-    _ptr_dbl,  # beta
-    _ptr_dbl,  # c
-    _ptr_int,  # ldc
-)
-_dsyrk = _dsyrk_functype(_dsyrk_addr)
-
-_dgemm_functype = ctypes.CFUNCTYPE(
-    None,
-    _ptr_int,  # transa
-    _ptr_int,  # transb
-    _ptr_int,  # m
-    _ptr_int,  # n
-    _ptr_int,  # k
-    _ptr_dbl,  # alpha
-    _ptr_dbl,  # a
-    _ptr_int,  # lda
-    _ptr_dbl,  # b
-    _ptr_int,  # ldb
-    _ptr_dbl,  # beta
-    _ptr_dbl,  # c
-    _ptr_int,  # ldc
-)
-_dgemm = _dgemm_functype(_dgemm_addr)
-
-_dpotrf_functype = ctypes.CFUNCTYPE(
-    None,
-    _ptr_int,  # uplo
-    _ptr_int,  # n
-    _ptr_dbl,  # a
-    _ptr_int,  # lda
-    _ptr_int,  # info
-)
-_dpotrf = _dpotrf_functype(_dpotrf_addr)
-
-_dpotri_functype = ctypes.CFUNCTYPE(
-    None,
-    _ptr_int,  # uplo
-    _ptr_int,  # n
-    _ptr_dbl,  # a
-    _ptr_int,  # lda
-    _ptr_int,  # info
-)
-_dpotri = _dpotri_functype(_dpotri_addr)
-
-_dpotrs_functype = ctypes.CFUNCTYPE(
-    None,
-    _ptr_int,  # uplo
-    _ptr_int,  # n
-    _ptr_int,  # nrhs
-    _ptr_dbl,  # a
-    _ptr_int,  # lda
-    _ptr_dbl,  # b
-    _ptr_int,  # ldb
-    _ptr_int,  # info
-)
-_dpotrs = _dpotrs_functype(_dpotrs_addr)
-
-_dgetrf_functype = ctypes.CFUNCTYPE(
-    None,
-    _ptr_int,  # m
-    _ptr_int,  # n
-    _ptr_dbl,  # a
-    _ptr_int,  # lda
-    _ptr_int,  # ipiv
-    _ptr_int,  # info
-)
-_dgetrf = _dgetrf_functype(_dgetrf_addr)
-
-_dgetri_functype = ctypes.CFUNCTYPE(
-    None,
-    _ptr_int,  # n
-    _ptr_dbl,  # a
-    _ptr_int,  # lda
-    _ptr_int,  # ipiv
-    _ptr_dbl,  # work
-    _ptr_int,  # lwork
-    _ptr_int,  # info
-)
-_dgetri = _dgetri_functype(_dgetri_addr)
+for symbol, (module, name) in _SYMBOLS.items():
+    ll.add_symbol(symbol, get_cython_function_address(module, name))
 
 
-@njit
+def _external_call_codegen(symbol_name, context, builder, signature, args):
+    ptrs = []
+    for arg_type, arg in zip(signature.args, args):
+        arr = context.make_array(arg_type)(context, builder, arg)
+        ptrs.append(arr.data)
+    fnty = ir.FunctionType(ir.VoidType(), [ptr.type for ptr in ptrs])
+    fn = cgutils.get_or_insert_function(builder.module, fnty, symbol_name)
+    builder.call(fn, ptrs)
+    return context.get_dummy_value()
+
+
+@intrinsic
+def _dsyrk_call(typingctx, uplo, trans, n, k, alpha, A, lda, beta, C, ldc):
+    sig = types.void(uplo, trans, n, k, alpha, A, lda, beta, C, ldc)
+
+    def codegen(context, builder, signature, args):
+        return _external_call_codegen("firth_dsyrk", context, builder, signature, args)
+
+    return sig, codegen
+
+
+@intrinsic
+def _dgemm_call(
+    typingctx, transa, transb, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc
+):
+    sig = types.void(transa, transb, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc)
+
+    def codegen(context, builder, signature, args):
+        return _external_call_codegen("firth_dgemm", context, builder, signature, args)
+
+    return sig, codegen
+
+
+@intrinsic
+def _dpotrf_call(typingctx, uplo, n, A, lda, info):
+    sig = types.void(uplo, n, A, lda, info)
+
+    def codegen(context, builder, signature, args):
+        return _external_call_codegen("firth_dpotrf", context, builder, signature, args)
+
+    return sig, codegen
+
+
+@intrinsic
+def _dpotri_call(typingctx, uplo, n, A, lda, info):
+    sig = types.void(uplo, n, A, lda, info)
+
+    def codegen(context, builder, signature, args):
+        return _external_call_codegen("firth_dpotri", context, builder, signature, args)
+
+    return sig, codegen
+
+
+@intrinsic
+def _dpotrs_call(typingctx, uplo, n, nrhs, A, lda, B, ldb, info):
+    sig = types.void(uplo, n, nrhs, A, lda, B, ldb, info)
+
+    def codegen(context, builder, signature, args):
+        return _external_call_codegen("firth_dpotrs", context, builder, signature, args)
+
+    return sig, codegen
+
+
+@intrinsic
+def _dgetrf_call(typingctx, m, n, A, lda, ipiv, info):
+    sig = types.void(m, n, A, lda, ipiv, info)
+
+    def codegen(context, builder, signature, args):
+        return _external_call_codegen("firth_dgetrf", context, builder, signature, args)
+
+    return sig, codegen
+
+
+@intrinsic
+def _dgetri_call(typingctx, n, A, lda, ipiv, work, lwork, info):
+    sig = types.void(n, A, lda, ipiv, work, lwork, info)
+
+    def codegen(context, builder, signature, args):
+        return _external_call_codegen("firth_dgetri", context, builder, signature, args)
+
+    return sig, codegen
+
+
+@njit(cache=True)
 def dsyrk(A: np.ndarray, C: np.ndarray) -> None:
     k = A.shape[0]
     n = A.shape[1]
 
-    uplo = np.array([ord("L")], dtype=np.int64)
-    trans = np.array([ord("N")], dtype=np.int64)
-    n_arr = np.array([k], dtype=np.int64)
-    k_arr = np.array([n], dtype=np.int64)
+    uplo = np.array([ord("L")], dtype=BLAS_FLAG_DTYPE)
+    trans = np.array([ord("N")], dtype=BLAS_FLAG_DTYPE)
+    n_arr = np.array([k], dtype=BLAS_INT_DTYPE)
+    k_arr = np.array([n], dtype=BLAS_INT_DTYPE)
     alpha = np.array([1.0], dtype=np.float64)
-    lda = np.array([k], dtype=np.int64)
+    lda = np.array([k], dtype=BLAS_INT_DTYPE)
     beta = np.array([0.0], dtype=np.float64)
-    ldc = np.array([k], dtype=np.int64)
+    ldc = np.array([k], dtype=BLAS_INT_DTYPE)
 
-    _dsyrk(
-        uplo.ctypes,
-        trans.ctypes,
-        n_arr.ctypes,
-        k_arr.ctypes,
-        alpha.ctypes,
-        A.ctypes,
-        lda.ctypes,
-        beta.ctypes,
-        C.ctypes,
-        ldc.ctypes,
-    )
+    _dsyrk_call(uplo, trans, n_arr, k_arr, alpha, A, lda, beta, C, ldc)
 
 
-@njit
+@njit(cache=True)
 def dgemm(A: np.ndarray, B: np.ndarray, C: np.ndarray) -> None:
     m = A.shape[0]
     k = A.shape[1]
     n = B.shape[1]
 
-    transa = np.array([ord("N")], dtype=np.int64)
-    transb = np.array([ord("N")], dtype=np.int64)
-    m_arr = np.array([m], dtype=np.int64)
-    n_arr = np.array([n], dtype=np.int64)
-    k_arr = np.array([k], dtype=np.int64)
+    transa = np.array([ord("N")], dtype=BLAS_FLAG_DTYPE)
+    transb = np.array([ord("N")], dtype=BLAS_FLAG_DTYPE)
+    m_arr = np.array([m], dtype=BLAS_INT_DTYPE)
+    n_arr = np.array([n], dtype=BLAS_INT_DTYPE)
+    k_arr = np.array([k], dtype=BLAS_INT_DTYPE)
     alpha = np.array([1.0], dtype=np.float64)
-    lda = np.array([m], dtype=np.int64)
-    ldb = np.array([k], dtype=np.int64)
+    lda = np.array([m], dtype=BLAS_INT_DTYPE)
+    ldb = np.array([k], dtype=BLAS_INT_DTYPE)
     beta = np.array([0.0], dtype=np.float64)
-    ldc = np.array([m], dtype=np.int64)
+    ldc = np.array([m], dtype=BLAS_INT_DTYPE)
 
-    _dgemm(
-        transa.ctypes,
-        transb.ctypes,
-        m_arr.ctypes,
-        n_arr.ctypes,
-        k_arr.ctypes,
-        alpha.ctypes,
-        A.ctypes,
-        lda.ctypes,
-        B.ctypes,
-        ldb.ctypes,
-        beta.ctypes,
-        C.ctypes,
-        ldc.ctypes,
+    _dgemm_call(
+        transa, transb, m_arr, n_arr, k_arr, alpha, A, lda, B, ldb, beta, C, ldc
     )
 
 
-@njit
+@njit(cache=True)
 def dpotrf(A: np.ndarray) -> int:
     n = A.shape[0]
-    uplo = np.array([ord("L")], dtype=np.int64)
-    n_arr = np.array([n], dtype=np.int64)
-    lda = np.array([n], dtype=np.int64)
-    info = np.array([0], dtype=np.int64)
+    uplo = np.array([ord("L")], dtype=BLAS_FLAG_DTYPE)
+    n_arr = np.array([n], dtype=BLAS_INT_DTYPE)
+    lda = np.array([n], dtype=BLAS_INT_DTYPE)
+    info = np.array([0], dtype=BLAS_INT_DTYPE)
 
-    _dpotrf(
-        uplo.ctypes,
-        n_arr.ctypes,
-        A.ctypes,
-        lda.ctypes,
-        info.ctypes,
-    )
+    _dpotrf_call(uplo, n_arr, A, lda, info)
     return int(info[0])
 
 
-@njit
+@njit(cache=True)
 def dpotri(L: np.ndarray) -> int:
     n = L.shape[0]
-    uplo = np.array([ord("L")], dtype=np.int64)
-    n_arr = np.array([n], dtype=np.int64)
-    lda = np.array([n], dtype=np.int64)
-    info = np.array([0], dtype=np.int64)
+    uplo = np.array([ord("L")], dtype=BLAS_FLAG_DTYPE)
+    n_arr = np.array([n], dtype=BLAS_INT_DTYPE)
+    lda = np.array([n], dtype=BLAS_INT_DTYPE)
+    info = np.array([0], dtype=BLAS_INT_DTYPE)
 
-    _dpotri(
-        uplo.ctypes,
-        n_arr.ctypes,
-        L.ctypes,
-        lda.ctypes,
-        info.ctypes,
-    )
+    _dpotri_call(uplo, n_arr, L, lda, info)
     return int(info[0])
 
 
-@njit
+@njit(cache=True)
 def dpotrs(L: np.ndarray, B: np.ndarray) -> int:
     n = L.shape[0]
     nrhs = B.shape[1]
-    uplo = np.array([ord("L")], dtype=np.int64)
-    n_arr = np.array([n], dtype=np.int64)
-    nrhs_arr = np.array([nrhs], dtype=np.int64)
-    lda = np.array([n], dtype=np.int64)
-    ldb = np.array([n], dtype=np.int64)
-    info = np.array([0], dtype=np.int64)
+    uplo = np.array([ord("L")], dtype=BLAS_FLAG_DTYPE)
+    n_arr = np.array([n], dtype=BLAS_INT_DTYPE)
+    nrhs_arr = np.array([nrhs], dtype=BLAS_INT_DTYPE)
+    lda = np.array([n], dtype=BLAS_INT_DTYPE)
+    ldb = np.array([n], dtype=BLAS_INT_DTYPE)
+    info = np.array([0], dtype=BLAS_INT_DTYPE)
 
-    _dpotrs(
-        uplo.ctypes,
-        n_arr.ctypes,
-        nrhs_arr.ctypes,
-        L.ctypes,
-        lda.ctypes,
-        B.ctypes,
-        ldb.ctypes,
-        info.ctypes,
-    )
+    _dpotrs_call(uplo, n_arr, nrhs_arr, L, lda, B, ldb, info)
     return int(info[0])
 
 
-@njit
+@njit(cache=True)
 def dgetrf(A: np.ndarray, ipiv: np.ndarray) -> int:
     m = A.shape[0]
     n = A.shape[1]
-    m_arr = np.array([m], dtype=np.int64)
-    n_arr = np.array([n], dtype=np.int64)
-    lda = np.array([m], dtype=np.int64)
-    info = np.array([0], dtype=np.int64)
+    m_arr = np.array([m], dtype=BLAS_INT_DTYPE)
+    n_arr = np.array([n], dtype=BLAS_INT_DTYPE)
+    lda = np.array([m], dtype=BLAS_INT_DTYPE)
+    info = np.array([0], dtype=BLAS_INT_DTYPE)
 
-    _dgetrf(
-        m_arr.ctypes,
-        n_arr.ctypes,
-        A.ctypes,
-        lda.ctypes,
-        ipiv.ctypes,
-        info.ctypes,
-    )
+    _dgetrf_call(m_arr, n_arr, A, lda, ipiv, info)
     return int(info[0])
 
 
-@njit
+@njit(cache=True)
 def dgetri(A: np.ndarray, ipiv: np.ndarray, work: np.ndarray) -> int:
     n = A.shape[0]
-    n_arr = np.array([n], dtype=np.int64)
-    lda = np.array([n], dtype=np.int64)
-    lwork = np.array([work.shape[0]], dtype=np.int64)
-    info = np.array([0], dtype=np.int64)
+    n_arr = np.array([n], dtype=BLAS_INT_DTYPE)
+    lda = np.array([n], dtype=BLAS_INT_DTYPE)
+    lwork = np.array([work.shape[0]], dtype=BLAS_INT_DTYPE)
+    info = np.array([0], dtype=BLAS_INT_DTYPE)
 
-    _dgetri(
-        n_arr.ctypes,
-        A.ctypes,
-        lda.ctypes,
-        ipiv.ctypes,
-        work.ctypes,
-        lwork.ctypes,
-        info.ctypes,
-    )
+    _dgetri_call(n_arr, A, lda, ipiv, work, lwork, info)
     return int(info[0])
